e87a56de54de6427df1982df57203a64e086be7f
sach10-create <65531346+sach10-create@users.noreply.github.com>
2021-09-25 19:22:28 +0530

    updated triangle-quiz.js

---
+++ triangle-quiz.js
@@ -0,0 +1,8 @@
+const quizForm = document.querySelector(".quiz-form");
+const checkButton = document.querySelector("#check-buttton");
+const output = document.querySelector("output-box");
+
+function getAnswers(){
+    
+}
+checkButton.addEventListener("click", getAnswers)

be253036d1d7cc7dd9a03a6fd11938c8dc69af73
sach10-create <65531346+sach10-create@users.noreply.github.com>
2021-09-25 20:38:43 +0530

    added the main logic

--- triangle-area.js
+++ triangle-area.js
@@ -1,8 +1,19 @@
-const base = document.querySelector(".base");
-const height = document.querySelector(".height");
+const sides = document.querySelectorAll(".sides");
 const button = document.querySelector(".calc-button")
 
-function calcArea(){
-    console.log("hi")
+function calcArea() {
+    const s = calcS(Number(sides[0].value), Number(sides[1].value), Number(sides[2].value));
+    const part1 = s - Number(sides[0].value);
+    const part2 = s - Number(sides[1].value);
+    const part3 = s - Number(sides[2].value);
+    const product = s * part1 * part2 * part3;
+    const area = Math.sqrt(product);
+    console.log(area);
 }
-button.addEventListener("click",calcArea)
+
+function calcS(a, b, c) {
+    var perimeter = a + b + c;
+    var s = perimeter / 2;
+    return s;
+}
+button.addEventListener("click", calcArea)

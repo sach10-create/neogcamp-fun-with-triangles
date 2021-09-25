1302760912f727fc9ce2ff4fb3db7bfdbb1c1a92
sach10-create <65531346+sach10-create@users.noreply.github.com>
2021-09-25 20:01:35 +0530

    updated the main logic

--- triangle-hypo.js
+++ triangle-hypo.js
@@ -2,13 +2,13 @@ const a = document.querySelector("#input1");
 const b = document.querySelector("#input2");
 const button = document.querySelector("#calc-button");
 
-function calcSumOfSquares(num1, num2){
-    const sum = (num1 * num1) +( num2 * num2);
+function calcSumOfSquares(num1, num2) {
+    const sum = (num1 * num1) + (num2 * num2);
     return sum;
 }
 
-function calcHypotenuse(){
-    var sumOfSquares = calcSumOfSquares(3 ,4);
+function calcHypotenuse() {
+    var sumOfSquares = calcSumOfSquares(a.value,b.value);
     console.log(sumOfSquares);
 }
 

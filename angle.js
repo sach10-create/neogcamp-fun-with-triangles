0a8248e11837889f9a0348c039ee77a3871c218d
sach10-create <65531346+sach10-create@users.noreply.github.com>
2021-09-25 19:18:10 +0530

    added quiz.html

--- angle.js
+++ angle.js
@@ -1,12 +1,17 @@
 const angles = document.querySelectorAll(".angle-input");
 const checkButton = document.querySelector("#check-button");
+const output = document.querySelector("#output-box");
 
-function checkTriangle(){
-    const sum = calculateSum(45,60,90);
-    console.log(sum);
+function checkTriangle() {
+    const sum = calculateSum(Number(angles[0].value), Number(angles[1].value), Number(angles[2].value));
+    if (sum === 180) {
+        output.innerText = "Your triangle is correct";
+    }else{
+        output.innerText = "Nayy!! Wrong Triangle";
+    }
 }
 
-function calculateSum(angle1, angle2, angle3){
+function calculateSum(angle1, angle2, angle3) {
     const sum = angle1 + angle2 + angle3;
     return sum;
 }

45e3ba5c8d442e58651a0bd46acedb4a2d19ddf6
sach10-create <65531346+sach10-create@users.noreply.github.com>
2021-09-25 19:53:15 +0530

    added triangle-hypotenuse.js

---
+++ triangle-hypo.js
@@ -0,0 +1,9 @@
+const a = document.querySelector("#input1");
+const b = document.querySelector("#input2");
+const button = document.querySelector("#calc-button");
+
+function calcHypotenuse(){
+    console.log("hi")
+}
+
+button.addEventListener("click", calcHypotenuse)

23d8aa9169a4370cab7ea8f4b5112d932e468b3c
sach10-create <65531346+sach10-create@users.noreply.github.com>
2021-09-25 20:43:31 +0530

    divided into proper functions

--- triangle-area.js
+++ triangle-area.js
@@ -1,14 +1,17 @@
 const sides = document.querySelectorAll(".sides");
 const button = document.querySelector(".calc-button")
+const output = document.querySelector(".output-box")
 
 function calcArea() {
     const s = calcS(Number(sides[0].value), Number(sides[1].value), Number(sides[2].value));
-    const part1 = s - Number(sides[0].value);
-    const part2 = s - Number(sides[1].value);
-    const part3 = s - Number(sides[2].value);
-    const product = s * part1 * part2 * part3;
+    const product = calcProduct(s,Number(sides[0].value), Number(sides[1].value), Number(sides[2].value))
     const area = Math.sqrt(product);
-    console.log(area);
+    output.innerText = "Area of Triangle "+ area;
+}
+
+function calcProduct(s,a,b,c){
+    const product = s * (s-a) * (s-b) * (s-c);
+    return product;
 }
 
 function calcS(a, b, c) {

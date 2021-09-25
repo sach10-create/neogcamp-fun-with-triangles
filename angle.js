87503eb0cacbb580e0f5840b76c1d64b06e2f0c3
sach10-create <65531346+sach10-create@users.noreply.github.com>
2021-09-25 19:00:18 +0530

    added javascript

---
+++ angle.js
@@ -0,0 +1,8 @@
+const angles = document.querySelectorAll(".angle-input");
+const checkButton = document.querySelector("#check-button");
+
+function calculateSum(angle1 , angle2 , angle3 ){
+     console.log("hi")
+}
+
+checkButton.addEventListener("click", calculateSum)

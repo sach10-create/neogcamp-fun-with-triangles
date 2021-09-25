52df2a940d8b7e7f05295c7b6663e3e737ec9568
sach10-create <65531346+sach10-create@users.noreply.github.com>
2021-09-25 20:18:27 +0530

    added triangle-area.js

---
+++ triangle-area.js
@@ -0,0 +1,8 @@
+const base = document.querySelector(".base");
+const height = document.querySelector(".height");
+const button = document.querySelector(".calc-button")
+
+function calcArea(){
+    console.log("hi")
+}
+button.addEventListener("click",calcArea)

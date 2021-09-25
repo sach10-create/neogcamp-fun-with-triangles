f44062078bb084431b79900b39c1c2e9550f5a83
sach10-create <65531346+sach10-create@users.noreply.github.com>
2021-09-25 20:03:31 +0530

    printed on the UI

--- triangle-hypo.js
+++ triangle-hypo.js
@@ -1,6 +1,7 @@
 const a = document.querySelector("#input1");
 const b = document.querySelector("#input2");
 const button = document.querySelector("#calc-button");
+const output = document.querySelector("#output-box");
 
 function calcSumOfSquares(num1, num2) {
     const sum = (num1 * num1) + (num2 * num2);
@@ -9,7 +10,8 @@ function calcSumOfSquares(num1, num2) {
 
 function calcHypotenuse() {
     var sumOfSquares = calcSumOfSquares(a.value,b.value);
-    console.log(sumOfSquares);
+    var hypo = Math.sqrt(sumOfSquares);
+    output.innerText = "Hypotenuse is " + hypo;
 }
 
 button.addEventListener("click", calcHypotenuse)
